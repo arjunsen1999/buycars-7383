6cdfce16f2007f9e7c636dad5fb19a0b6d3fe237
arjunsen1999 <arjunsen717@gmail.com>
2023-05-24 15:26:26 +0530

    added auth signup controller

---
+++ Server/controller/auth/Signup.controller.js
@@ -0,0 +1,55 @@
+const { AuthModel } = require("../../models/Auth.model");
+const asyncHandler = require("express-async-handler");
+const bcrypt = require("bcrypt");
+const saltRounds = 6;
+
+const SignupController = asyncHandler(async (req, res) => {
+  try {
+    const { first_name, last_name, role, email, password } = req.body;
+    const reqData = {
+      first_name,
+      last_name,
+      email,
+      password,
+    };
+    // if role exists
+    if (role) {
+      reqData.role = role;
+    }
+
+    // Check if user with the same email already exists
+    const existingUser = await AuthModel.findOne({ email });
+    if (existingUser) {
+      return res.status(409).json({ message: "email already exists" });
+    }
+
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+    reqData.password = hashedPassword;
+
+    // Create a new user
+    const newUser = new AuthModel(reqData);
+
+    // Save the user to the database
+    await newUser.save();
+
+    // Create response
+    const response = {
+      status: "success",
+      message: "Signup successful",
+    };
+    res.status(201).json(response);
+  } catch (error) {
+    // Create error response
+    const error_response = {
+      status: "fail",
+      message: "An error occurred",
+    };
+    res.status(500).json(error_response);
+    console.log(error);
+  }
+});
+
+module.exports = {
+  SignupController,
+};
