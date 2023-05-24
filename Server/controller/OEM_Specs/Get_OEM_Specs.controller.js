7be63b17a7589911154bb98ac1c4e56697db28cf
arjunsen1999 <arjunsen717@gmail.com>
2023-05-24 17:39:06 +0530

    added OEM_Specs get and post controller

---
+++ Server/controller/OEM_Specs/Get_OEM_Specs.controller.js
@@ -0,0 +1,30 @@
+const { OEM_Specs_Model } = require("../../models/OEM_Specs.model");
+const asyncHandler = require("express-async-handler");
+
+const Get_OEM_SpecsController = asyncHandler(async (req, res) => {
+  try {
+    const { search } = req.query;
+
+    const OEM_Specs_Data = await OEM_Specs_Model.find();
+
+    // Create Response
+    const response = {
+      status: "success",
+      data: OEM_Specs_Data,
+    };
+    // Send Response
+    return res.status(200).json(response);
+  } catch (error) {
+    // Create error response
+    const error_response = {
+      status: "fail",
+      message: "An error occurred",
+    };
+    console.log(error);
+    return res.status(500).json(error_response);
+  }
+});
+
+module.exports = {
+    Get_OEM_SpecsController,
+}
